33680b7574dddd30b3a68672c31c6405d57f4412
Luis Fernando Javier Velazquez Sosa <60881791+l-velazquez@users.noreply.github.com>
2024-05-13 14:42:34 -0400

    chore: Update get_menus function to retrieve menus from API
    This commit modifies the `get_menus` function in the `Menus` views module to retrieve menus from the API using the provided `id`. It also adds error handling for cases where the user is not authenticated or the API request fails.

--- UniFood/Menus/views.py
+++ UniFood/Menus/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+debug = os.getenv('DJANGO_DEBUG')
+
+api_url = os.getenv('API_URL')
 
 # Create your views here.
+
+def get_menus(id,request):
+    token = request.session.get('jwt')
+    if not token:
+        return render(request, 'login.html')
+    
+    # Get all menus from API
+    headers = {'Authorization': f'Bearer {token}'}
+
+    response = requests.get(api_url + f'/menus/{id}/', headers=headers)
+
+    if debug:
+        print(f'URL: {api_url}')
+        print(f'Status code: {response.status_code}')
+        print(f'Response: {response.text}')
+    
+    menus = response.json()
+
+    return menus
+
+    
