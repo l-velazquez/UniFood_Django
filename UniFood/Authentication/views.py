a0879d78510501b80da847468bbf96ea068b1eb8
Luis Fernando Javier Velazquez Sosa <60881791+l-velazquez@users.noreply.github.com>
2024-05-13 14:42:22 -0400

    chore: Update login view to send credentials to API
    This commit updates the login view in the Authentication app to send the user's email and password as credentials to the API. It also saves the JWT token from the API response to the Django session.

--- UniFood/Authentication/views.py
+++ UniFood/Authentication/views.py
@@ -1,10 +1,61 @@
 from django.shortcuts import render
 import requests
+from dotenv import load_dotenv
+import os
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
 
+load_dotenv()
+debug = os.getenv('DJANGO_DEBUG')
+
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # For debugging purposes
+        debug = True  # Set to False in production
+        if debug:
+            print(f'Email: {email}')
+            print(f'Password: {password}')
+
+        # Send credentials to API
+        api_url = os.getenv('API_URL') + 'Login/'
+
+        data = {
+            'email': email,
+            'password': password
+        }
+        headers = {
+            'Content-Type': 'application/json',
+            'ApiKey': os.getenv('API_KEY')
+        }
+        response = requests.post(api_url, json=data, headers=headers)
+
+        if debug:
+            print(f'URL: {api_url}')
+            print(f'Status code: {response.status_code}')
+            print(f'Response: {response.text}')  # Changed to print the text of the response
+        
+        #save the response JWT to django session
+        token = response.text
+        request.session['jwt'] = token
+
+        if response.status_code == 200:
+            # Successful login
+            messages.success(request, 'You have successfully logged in')
+            return redirect("/home")
+        elif response.status_code == 400:
+            messages.error(request, 'Invalid email or password')
+            return render(request, 'login.html')
+        elif response.status_code == 404:
+            return render(request, 'error/404.html')
+        else:
+            # Failed login
+            return render(request, 'failure.html')
     
     return render(request, 'login.html')
 
