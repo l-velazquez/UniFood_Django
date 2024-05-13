c4f4b9f4e800e65c271ffe013313dbcdf0ea0022
Luis Fernando Javier Velazquez Sosa <60881791+l-velazquez@users.noreply.github.com>
2024-05-13 14:43:50 -0400

    feat: Add API integration for retrieving universities
    This commit adds API integration to retrieve universities from the backend API. The `get_all_universities` view now makes a GET request to the API endpoint `/Universities/` with the user's JWT token for authorization. The response is then rendered in the `Universities.html` template. Additionally, a new view `get_university` is added to retrieve a specific university by its ID.

--- UniFood/Universities/views.py
+++ UniFood/Universities/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+import requests
+from dotenv import load_dotenv
+import os
+from django.contrib import messages
+
+debug = os.getenv('DJANGO_DEBUG')
 
 # Create your views here.
+
+load_dotenv()
+api_url = os.getenv('API_URL')
+
+def get_all_universities(request):
+    token = request.session.get('jwt')
+
+    if not token:
+        return render(request, 'login.html')
+    
+    # Get all universities from API
+    headers = {'Authorization': f'Bearer {token}'}
+
+    response = requests.get(api_url + '/Universities/', headers=headers)
+
+    if debug:
+        print(f'URL: {api_url}/Universities/')
+        print(f"Token: {token}")
+        print(f'Status code: {response.status_code}')
+        print(f'Response: {response.text}')
+    if response.status_code == 401:
+        messages.error(request, 'You are not authorized to view this page. Please login.')
+        return render(request, 'login.html')
+    
+    elif response.status_code == 404:
+        return render(request, 'error/404.html')
+
+    universities = response
+    return render(request, 'Universities.html', {'universities': universities})
+
+def get_university(request, university_id):
+    token = request.session.get('jwt')
+    # Get all universities from API
+    headers = {'Authorization': f'Bearer {token}'}
+
+    # Get university from API
+    response = requests.get(api_url + f'/universities/{university_id}/', headers=headers)   
+    university = response.json()
+
+    if debug:
+        print(f'URL: {api_url}')
+        print(f'Status code: {response.status_code}')
+        print(f'Response: {response.text}')
+
+    return render(request, 'university.html', {'university': university})
+    
