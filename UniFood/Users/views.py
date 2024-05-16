ce5d038593bc445c2064c4a910dd9db6999d19eb
Luis Fernando Javier Velazquez Sosa <60881791+l-velazquez@users.noreply.github.com>
2024-05-16 13:09:19 -0400

    feat: Add profile page template
    This commit adds the profile.html template, which extends the base.html template and displays the user's profile information. The template includes sections for the user's name, email, university, and favorite places. It also includes an edit button for easy profile editing. This addition enhances the user experience by providing a dedicated page for viewing and managing user profiles.

--- UniFood/Users/views.py
+++ UniFood/Users/views.py
@@ -22,17 +22,8 @@ def home(request):
         'ApiKey': f'{api_key}',
         'Authorization': f'Bearer {token}',
     }
-
-    if debug:
-        print(f'URL: {api_url}Users/email/{token_user_email}')
-        print(f"Token: {token}")
-        print(f'API Key: {api_key}')
-
+    
     get_user_by_email = requests.get(api_url + f'Users/email/{token_user_email}', headers=headers, verify=False)
-
-    if debug:
-        print(f'Status code: {get_user_by_email.status_code}')
-        print(f'Response: {get_user_by_email.text}')
 
     users_name = get_user_by_email.json()
     users_name = users_name['firstName']
@@ -77,3 +68,41 @@ def get_all_users(request):
             print(f'Context: {extra_context}')
 
         return render(request, 'Users.html', extra_context)
+    
+
+def profile(request):
+    token = request.session.get('jwt')
+    if not token:
+        return render(request, 'login.html')
+    
+    token_data = jwt.decode(token, verify=False, algorithms=['HS256'], options={"verify_signature": False})
+    token_user_email = token_data['email']
+
+    headers = {
+        'ApiKey': f'{api_key}',
+        'Authorization': f'Bearer {token}',
+    }
+    
+    get_user_by_email = requests.get(api_url + f'Users/email/{token_user_email}', headers=headers, verify=False)
+
+    user = get_user_by_email.json()
+    uni_id = user['universityId']
+    uni_name = requests.get(api_url + f'Universities/{uni_id}', headers=headers, verify=False)
+    university = uni_name.json()
+    if debug:
+        print(f'URL: {api_url}/Universities/{uni_id}')
+        print(f'Status code: {uni_name.status_code}')
+        print(f'Response: {uni_name.text}')
+    
+    if uni_name.status_code == 400:
+        university = {'name': 'University not found'}
+        
+    elif uni_name.status_code == 401:
+        messages.error(request, 'You are not authorized to view this page. Please login.')
+        return render(request, 'login.html')
+    
+    
+    extra_context = {'user': user, 'university': university}
+
+    
+    return render(request, 'profile.html', extra_context)
