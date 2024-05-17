ed0f5108bda868a67e792d0b4a873080123cb7a8
Luis Fernando Javier Velazquez Sosa <60881791+l-velazquez@users.noreply.github.com>
2024-05-17 14:55:12 -0400

    feat: Add form to set place as favorite in Menu.html
    This commit adds a form to the Menu.html template that allows users to set a place as their favorite. The form includes a button that triggers a POST request to the '/set_favorite' endpoint with the place ID as a parameter. This feature enhances the user experience by providing an easy way to add places to their favorites list.

--- UniFood/Menus/views.py
+++ UniFood/Menus/views.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 from django.contrib import messages
 
+
 load_dotenv()
 debug = os.getenv('DJANGO_DEBUG')
 
@@ -13,19 +14,19 @@ def get_menus(request, id, university_id):
     token = request.session.get('jwt')
     if not token:
         return render(request, 'login.html')
-    
-    # Get all menus from API
     headers = {'Authorization': f'Bearer {token}',
                'ApiKey': os.getenv('API_KEY')
-               }
+               } 
+
+    # Get all menus from API
     response = requests.get(api_url + f'menus/{id}/', headers=headers, verify=False)
     place_name = requests.get(api_url + f'places/{university_id}?placeId={id}', headers=headers, verify=False)
     place = place_name.json()
 
-    if debug:
-        print(f'URL: {api_url}menus/{id}/')
-        print(f'Status code: {response.status_code}')
-        print(f'Response: {response.text}')
+    # if debug:
+    #     print(f'URL: {api_url}menus/{id}/')
+    #     print(f'Status code: {response.status_code}')
+    #     print(f'Response: {response.text}')
     
     menus = response.json()
     if response.status_code == 401:
