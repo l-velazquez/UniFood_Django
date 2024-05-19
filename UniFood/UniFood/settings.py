6b0e7ec45cc5c01981444e268c2a99956090d3ba
Luis Fernando Javier Velazquez Sosa <60881791+l-velazquez@users.noreply.github.com>
2024-05-19 17:00:31 -0400

    Update DEBUG setting to handle boolean value correctly

--- UniFood/UniFood/settings.py
+++ UniFood/UniFood/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG")
+DEBUG = os.getenv("DJANGO_DEBUG") == 'True'
 print(f'Debug: {DEBUG}')
 
 ALLOWED_HOSTS = ['*']
